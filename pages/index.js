d1ac212270601809a9124715b6a6bdd4ebf717a5
shubhamjain <subham.jain@grootify.com>
2021-05-17 18:51:27 +0530

    hero bg try of figma

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
+// import herobg from '../public/herobg.png'
 
 export default function Home() {
   return (
@@ -11,11 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto max-w-7xl">
-        <h1 className="text-5xl text-red-500" >
+      <main className="container  h-screen bg-cover bg-no-repeat" style={{backgroundImage:`url(/herobg.png)`}}>
+        {/* <h1 className="text-5xl text-red-500" >
           Welcome to my portfolio
-        </h1>
-        <h1 className="text-3xl"> this is the teting purpose text </h1>
+        </h1> */}
+        {/* <h1 className="text-3xl"> this is the teting purpose text </h1> */}
        </main>
     </div>
   )
